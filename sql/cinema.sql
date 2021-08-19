4be196fd19178997c616f347e4fc8d8464e3ebfb
Eash10 <eashaan.vyas@gmail.com>
2021-08-19 12:08:53 +0100

    added foodrink

--- sql/cinema.sql
+++ sql/cinema.sql
@@ -18,12 +18,22 @@ CREATE TABLE film (
     PRIMARY KEY (film_id)
 );
 
+CREATE TABLE foodrink (
+    foodrink_id INT AUTO_INCREMENT,
+    size_of_drink CHAR(1) NOT NULL,
+    size_of_popcorn CHAR(1) NOT NULL,
+    price DECIMAL NOT NULL,
+    PRIMARY KEY (foodrink_id)
+);
+
 CREATE TABLE booking (
     booking_id INT AUTO_INCREMENT,
     fk_customer_id INT,
     fk_film_id INT,
+    fk_foodrink_id INT,
     date_booking DATETIME NOT NULL,
     PRIMARY KEY (booking_id),
     FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (fk_film_id) REFERENCES film(film_id)    
+    FOREIGN KEY (fk_film_id) REFERENCES film(film_id),
+    FOREIGN KEY (fk_foodrink_id) REFERENCES foodrink(foodrink_id)    
 );
