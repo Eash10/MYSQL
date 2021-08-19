89516d31844ea13828d9135e2bc83a85c0f99810
Eash10 <eashaan.vyas@gmail.com>
2021-08-19 11:12:27 +0100

    adding film table

--- sql/cinema.sql
+++ sql/cinema.sql
@@ -6,8 +6,16 @@ CREATE TABLE customer (
     customer_id INT AUTO_INCREMENT,
     customer_name VARCHAR(50) NOT NULL,
     customer_address VARCHAR(50) NOT NULL,
-    customer_age SMALLINT NOT NULL,
+    age SMALLINT NOT NULL,
     PRIMARY KEY (customer_id)
+);
+
+CREATE TABLE film (
+    film_id INT AUTO_INCREMENT,
+    film_name VARCHAR(100) NOT NULL,
+    rating VARCHAR(2) NOT NULL,
+    runtime TIME NOT NULL,
+    PRIMARY KEY (film_id)
 );
 
 CREATE TABLE booking (
@@ -15,5 +23,6 @@ CREATE TABLE booking (
     fk_customer_id INT,
     date_booking DATETIME NOT NULL,
     PRIMARY KEY (booking_id),
-    FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id)    
+    FOREIGN KEY (fk_customer_id) REFERENCES customer(customer_id),
+    FOREIGN KEY (fk_film_id) REFERENCES film(film_id)    
 );
